4e534997234fb3c55a036cd26d6ffd75079b7051
pkominlmu <75468547+pkominlmu@users.noreply.github.com>
2021-09-12 05:55:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const principalBox = document.querySelector("#principal");
+const interestBox = document.querySelector("#intRate");
+const numTimesCompoundedBox = document.querySelector("#timesCompounded");
+const numYearsBox = document.querySelector("#numYears");
+const compInterestSpan = document.querySelector("#compInterest");
+const totalAmountSpan = document.querySelector("#totalEarned");
+
+function computeCompInterest() {
+  const principal = principalBox.value;
+  const interest = interestBox.value;
+  const numTimesCompounded = numTimesCompoundedBox.value;
+  const numYears = numYearsBox.value;
+  const totalAmount = (principal * (Math.pow((1 + ((interest/100)/numTimesCompounded)), (numTimesCompounded * numYears))));
+  const compInterest = totalAmount - principal;
+  compInterestSpan.textContent = compInterest;
+  totalAmountSpan.textContent = totalAmount;
+}
+
+principalBox.addEventListener('input', computeCompInterest);
+interestBox.addEventListener('input', computeCompInterest);
+numTimesCompoundedBox.addEventListener('input', computeCompInterest);
+numYearsBox.addEventListener('input', computeCompInterest);
+
